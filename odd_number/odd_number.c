07a090332a6ae7ed9633cecd60caea798a27159d
niloyraytridib <niloyraytridip849@gmail.com>
2025-07-19 15:35:03 +0600

    Add files via upload

---
+++ odd_number/odd_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+   int n;
+   //input a number...
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   //using for loop...
+   for(int i=1; i<=n; i=i+2) {
+       printf("%d ", i);
+       }
+   return 0;
+}
