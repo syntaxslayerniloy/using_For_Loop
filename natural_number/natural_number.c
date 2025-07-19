317c63075d37dcaa6ca5585f95165091761e852c
niloyraytridib <niloyraytridip849@gmail.com>
2025-07-19 15:33:49 +0600

    Add files via upload

---
+++ natural_number/natural_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+   int n;
+   //input a number...
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   //using for loop...
+   for(int i=0; i<=n; i++) {
+       printf("%d ",i);
+    }
+    return 0;
+}
+
